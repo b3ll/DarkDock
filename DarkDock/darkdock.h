9a5efa54585f5f0e7005baf9fa9cf1d44ee58700
Adam Bell <adam@adambell.me>
2014-07-06 10:57:47 -0700

    Initial commit

---
+++ DarkDock/darkdock.h
@@ -0,0 +1,13 @@
+//
+//  darkdock.h
+//  darkdock
+//
+//  Created by Adam Bell on 2014-07-05.
+//  Copyright (c) 2014 Adam Bell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface darkdock : NSObject
+
+@end
